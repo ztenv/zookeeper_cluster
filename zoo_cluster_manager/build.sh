cdba48a26a163a129874a9ffae23e52deccc7205
shaohua.lian <shaohua.lian@xuncetech.com>
2019-05-07 11:20:45 +0800

    add:include file and lib file

--- zoo_cluster_manager/build.sh
+++ zoo_cluster_manager/build.sh
@@ -1,1 +1,1 @@
-g++ -Wall -g main.cpp cluster_manager.cpp -o main -DTHREADED -I/usr/include/zookeeper -lzookeeper_mt --std=c++11
+g++ -Wall -g main.cpp cluster_manager.cpp -o main -DTHREADED -I./include/zookeeper -L./lib -lzookeeper_mt --std=c++11
--- zoo_cluster_manager/main.cpp
+++ zoo_cluster_manager/main.cpp
@@ -36,7 +36,8 @@ int main(int argc,char *argv[])
 
     IEventHandlerPtr ehPtr=IEventHandlerPtr(new EventHandler());
 
-    icmPtr->Initialize("192.168.0.227:2181,192.168.0.227:2182,192.168.0.227:2183",
+    //icmPtr->Initialize("192.168.0.227:2181,192.168.0.227:2182,192.168.0.227:2183",
+    icmPtr->Initialize("192.168.0.223:2181,192.168.0.224:2181,192.168.0.225:2181",
                        path,node,100);
     icmPtr->Register(ehPtr);
     icmPtr->Start();
