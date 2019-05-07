37600d3734bdeaab7627c50239c9402e6fc6956b
shaohua.lian <shaohua.lian@xuncetech.com>
2019-05-07 12:08:32 +0800

    fix:build.sh

--- zoo_cluster_manager/build.sh
+++ zoo_cluster_manager/build.sh
@@ -1,1 +1,3 @@
-g++ -Wall -g main.cpp cluster_manager.cpp -o main -DTHREADED -I./include/zookeeper -L./lib -lzookeeper_mt --std=c++11
+g++ -Wall -g -fPIC -shared cluster_manager.cpp -o libcluster.so -DTHREADED -I./include/zookeeper -L./lib -lzookeeper_mt --std=c++11
+mv libcluster.so ./lib
+g++ -Wall -g main.cpp -o main -DTHREADED -I./include/zookeeper -L./lib -lzookeeper_mt -lcluster --std=c++11
