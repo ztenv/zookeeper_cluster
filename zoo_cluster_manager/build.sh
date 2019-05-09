57c585d650e1dd9b1ee4f98529fa852da2ea2e0c
shaohua.lian <shaohua.lian@xuncetech.com>
2019-05-09 11:15:16 +0800

    fix:rename libcluster.so to libcluster_t.so

--- zoo_cluster_manager/build.sh
+++ zoo_cluster_manager/build.sh
@@ -1,3 +1,2 @@
-g++ -Wall -g -fPIC -shared cluster_manager.cpp -o libcluster.so -DTHREADED -I./include/zookeeper -L./lib -lzookeeper_mt --std=c++11
-mv libcluster.so ./lib
-g++ -Wall -g main.cpp -o main -DTHREADED -I./include/zookeeper -L./lib -lzookeeper_mt -lcluster --std=c++11
+g++ -Wall -g -fPIC -shared cluster_manager.cpp -o ./lib/libcluster_t.so -DTHREADED -I./include/zookeeper -L./lib -lzookeeper_mt --std=c++11
+g++ -Wall -g main.cpp -o main -DTHREADED -I./include/zookeeper -L./lib -lzookeeper_mt -lcluster_t --std=c++11
